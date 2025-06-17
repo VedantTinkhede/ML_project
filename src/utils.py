2e27a289c61f256a0b4a75b85d08e601804b4a1b
VedantTinkhede <vedanttinkhede797@gmail.com>
2025-06-17 18:46:59 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 import dill # dill is used to serialize and deserialize Python objects and creating a pickle file
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -47,5 +48,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
         
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
